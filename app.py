e3754db930dd50fe980c34bb7adade48dab92132
samuelslv <samuelslv7@gmail.com>
2026-04-19 21:47:20 -0300

    add a rota DEBUG

--- app.py
+++ app.py
@@ -56,6 +56,26 @@ def index():
     return render_template("index.html", resultados=resultados, siape=siape_buscado)
 
 
+@app.route('/debug')
+def debug_files():
+    import os
+    # Lista o que tem na pasta static/pdfs
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    pdf_path = os.path.join(base_dir, 'static', 'portarias')
+    
+    files = []
+    if os.path.exists(pdf_path):
+        files = os.listdir(pdf_path)
+    
+    return {
+        "diretorio_atual": base_dir,
+        "caminho_buscado": pdf_path,
+        "pasta_existe": os.path.exists(pdf_path),
+        "arquivos_na_pasta": files
+    }
+
+
+
 if __name__ == "__main__":
     # migrar_excel_para_sqlite_ajustado("portarias.xlsx", DB_PATH)
     app.run(debug=True, host="0.0.0.0", port=5000)
