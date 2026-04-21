5736a4c03fb500bab9763112ed61dee311275eee
samuelslv <samuelslv7@gmail.com>
2026-04-21 10:48:58 -0300

    Finalizado backend

--- sync_drive.py
+++ sync_drive.py
@@ -3,15 +3,13 @@ from drive_service import obter_servico_drive
 
 def sincronizar_links_drive():
     service = obter_servico_drive()
-    # Este agora é o ID da PASTA PAI (que contém as pastas 2017, 2018, etc.)
+
     ID_PASTA_RAIZ = "1bMndtRFMbucRqEqjJK3Hl3LRT9N-DAcc"
     
-    # 1. Primeiro, vamos listar todas as SUBPASTAS da pasta raiz
     query_pastas = f"'{ID_PASTA_RAIZ}' in parents and mimeType = 'application/vnd.google-apps.folder' and trashed = false"
     pastas_results = service.files().list(q=query_pastas, fields="files(id, name)").execute()
     subpastas = pastas_results.get('files', [])
     
-    # Adicionamos a própria pasta raiz na lista, caso haja arquivos nela também
     ids_para_buscar = [ID_PASTA_RAIZ] + [f['id'] for f in subpastas]
     
     conn = sqlite3.connect("./db/contratos_ufpi.db")
@@ -21,7 +19,7 @@ def sincronizar_links_drive():
     print(f"Buscando em {len(ids_para_buscar)} pastas diferentes...")
 
     for pasta_id in ids_para_buscar:
-        # 2. Busca os arquivos dentro de cada subpasta
+        # Busca os arquivos dentro de cada subpasta
         query_files = f"'{pasta_id}' in parents and trashed = false"
         results = service.files().list(q=query_files, fields="files(name, webViewLink)").execute()
         arquivos_drive = results.get('files', [])
